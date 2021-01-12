7393acfe9ddda64a1b201aa953e3396915115bea
Tom2rec <t.turek2000@gmail.com>
2021-01-12 19:38:00 +0100

    Adding user accounts dealing

--- books/views.py
+++ books/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from books.models import Book
-
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     return render(request, "index.html")
@@ -16,3 +15,17 @@ def book_details(request, book_id):
     context = {"book": Book.objects.get(id=book_id)}
     return render(request, "book_details.html", context)
 
+
+def user_signup(request):
+    form = None
+    if request.method == "POST":
+        # create userdd
+        UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "registration/singup_completed.html")
+    else:
+        #show the empty form
+        form = UserCreationForm()
+        context = {"form":form}
+    return render(request, "registration/signup_form.html", context)
